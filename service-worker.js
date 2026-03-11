941a73707654786caa83eccd360e109b5ab477e0
henriqueab46-lab <henriqueab46@gmail.com>
2026-03-11 10:13:13 -0300

    Update service

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,24 @@
-self.addEventListener("install", function(event) {
-console.log("App instalado");
+const CACHE_NAME = "estoque-app-v1";
+
+const urlsToCache = [
+"/controle-estoque/",
+"/controle-estoque/index.html"
+];
+
+self.addEventListener("install", event => {
+event.waitUntil(
+caches.open(CACHE_NAME)
+.then(cache => {
+return cache.addAll(urlsToCache);
+})
+);
 });
 
-self.addEventListener("fetch", function(event) {
-event.respondWith(fetch(event.request));
+self.addEventListener("fetch", event => {
+event.respondWith(
+caches.match(event.request)
+.then(response => {
+return response || fetch(event.request);
+})
+);
 });
